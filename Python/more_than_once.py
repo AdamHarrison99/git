8e79db90f8264611453ba143290bde3a5e31d05c
Adam <adamscotth@gmail.com>
2022-05-11 10:49:13 -0600

    test

--- Python/more_than_once.py
+++ Python/more_than_once.py
@@ -2,19 +2,20 @@ import random as rand
 
 
 if __name__ == '__main__':
-
+    #settup vars
     n = []
     occur_more_than_once = []
 
     for i in range(10):
         for j in range(rand.randint(0,5)):
             n.append(i)
-
+    #We do this so that 0 is not counted each time
     last_int = ' '
     occur_more_than_once.append('start')
 
     for i in n:
         if i == last_int:
+            #last var in occur_more_than_once 
             if occur_more_than_once[-1] != i:
                 occur_more_than_once.append(i)
         last_int = i
