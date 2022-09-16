2cf0b30d219c09bad459ec60198835a22ecf1554
AdamHarrison99 <adamscotth@gmail.com>
2022-09-16 17:29:49 -0600

    finished UI (mostly)

--- JavaScript/polygonal art/README.txt
+++ JavaScript/polygonal art/README.txt
@@ -1,1 +1,2 @@
 program that generates polygonal style art from user input
+use black and white images with strong outline for best results, or see example input images
--- JavaScript/polygonal art/js/script.js
+++ JavaScript/polygonal art/js/script.js
@@ -4,6 +4,7 @@ var colorSlider = document.querySelector(".colorSlider");
 var sizeVal = document.getElementById("sizeValueNum");
 var complexityVal = document.getElementById("complexityValueNum");
 var colorVal = document.getElementById("colorValueNum");
+var imageCanvas = document.getElementById("image")
 
 sizeVal.innerHTML = sizeSlider.value;
 complexityVal.innerHTML = complexitySlider.value;
@@ -19,4 +20,17 @@ complexitySlider.oninput = function() {
 
 colorSlider.oninput = function() {
   colorVal.innerHTML = colorSlider.value;
+}
+
+var loadImage = function(event){
+  var imageUpload = document.getElementById("file-upload");
+  imageCanvas.src = URL.createObjectURL(event.target.files[0]);
+}
+
+function save(event){
+  return 0;
+}
+
+function reset(event){
+  location.reload();
 }
