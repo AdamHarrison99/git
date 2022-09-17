188c8393f262e3d4442e6e4c0bddc265bb95d3d9
AdamHarrison99 <adamscotth@gmail.com>
2022-09-17 17:39:42 -0600

    polygons displaying

--- JavaScript/polygonal art/README.txt
+++ JavaScript/polygonal art/README.txt
@@ -1,2 +1,3 @@
 program that generates polygonal style art from user input
 use black and white images with strong outline for best results, or see example input images
+uses triangular.js (https://maxwellito.github.io/triangulr/)
--- JavaScript/polygonal art/js/script.js
+++ JavaScript/polygonal art/js/script.js
@@ -5,27 +5,51 @@ var sizeVal = document.getElementById("sizeValueNum");
 var complexityVal = document.getElementById("complexityValueNum");
 var colorVal = document.getElementById("colorValueNum");
 var imageCanvas = document.getElementById("image")
+var board = document.querySelector(".board");
+
+var colorGenerator = function (path) { //TODO randomness from colorSlider.value as seed. Lower colorSlider.value = less color change. Higher colorSlider.value = large color change between polygons
+  var random = Number(colorSlider.value);
+  var ratio = (path.x * path.y) / (path.cols * path.lines);
+  var code = Math.floor(255 - (ratio * (255-random)) - Math.random()*random).toString(16);
+  /*return '#'+code+code+code;*/
+  return '#'+Math.floor(Math.random()*16777215).toString(16);
+};
 
 sizeVal.innerHTML = sizeSlider.value;
 complexityVal.innerHTML = complexitySlider.value;
 colorVal.innerHTML = colorSlider.value;
+var mySVG = new Triangulr (500, 500, Number(sizeSlider.value), Number(complexitySlider.value), colorGenerator);
+board.appendChild(mySVG);
 
 sizeSlider.oninput = function() {
   sizeVal.innerHTML = sizeSlider.value;
+  optionsUpdate()
 }
 
 complexitySlider.oninput = function() {
   complexityVal.innerHTML = complexitySlider.value;
+  optionsUpdate()
 }
 
 colorSlider.oninput = function() {
   colorVal.innerHTML = colorSlider.value;
+  optionsUpdate()
+}
+
+function optionsUpdate(){
+  board.removeChild(board.firstElementChild);
+  var mySVG = new Triangulr (500, 500, Number(sizeSlider.value), Number(complexitySlider.value), colorGenerator);
+  board.appendChild(mySVG);
 }
 
 var loadImage = function(event){
   var imageUpload = document.getElementById("file-upload");
   imageCanvas.src = URL.createObjectURL(event.target.files[0]);
 }
+
+window.addEventListener("resize", function(event) {
+  console.log(document.body.clientWidth + ' wide by ' + document.body.clientHeight+' high');
+})
 
 function save(event){
   return 0;
